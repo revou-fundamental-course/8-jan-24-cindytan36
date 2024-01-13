2e195d4cf1ba22325f03dc43d55a81ec9997c7c1
CINDY <cindytanjaya3@gmail.com>
2024-01-13 20:40:51 +0700

    first push

---
+++ Katik/js/script.js
@@ -0,0 +1,49 @@
+let rumusHitungLuas = document.getElementById("rumus-hitung-luas");
+let rumusLuas = document.getElementById("rumus-luas");
+let hasilLuas = document.getElementById("hasil-luas");
+
+let rumusHitungKeliling = document.getElementById("rumus-hitung-keliling");
+let rumusKeliling = document.getElementById("rumus-keliling");
+let hasilKeliling = document.getElementById("hasil-keliling");
+
+// function Hitung Luas Persegi
+
+function hitungLuas() {
+  let inputSisiLuas = document.getElementById("input-sisi-luas").value;
+  const luas = inputSisiLuas * inputSisiLuas;
+  rumusHitungLuas.textContent = `L = S x S`;
+  rumusLuas.textContent = `L = ${inputSisiLuas} * ${inputSisiLuas}`;
+  hasilLuas.textContent = `L = ${luas} cm²`;
+}
+
+// function Reset Luas Persegi
+
+function resetLuas() {
+  document.getElementById("rumus-hitung-luas").innerHTML = "";
+  document.getElementById("rumus-luas").innerHTML = "";
+  document.getElementById("hasil-luas").innerHTML = "";
+}
+
+// function Hitung Keliling Persegi
+
+function hitungKeliling() {
+  let inputSisiKeliling = document.querySelector("#input-sisi-keliling").value;
+  let keliling = 4 * inputSisiKeliling;
+  rumusHitungKeliling.textContent = `K = 4 x S`;
+  rumusKeliling.textContent = `L = 4 * ${inputSisiKeliling}`;
+  hasilKeliling.textContent = `L = ${keliling} cm`;
+}
+
+// function Reset Keliling Persegi
+
+function resetKeliling() {
+  document.getElementById("rumus-hitung-keliling").innerHTML = "";
+  document.getElementById("rumus-keliling").innerHTML = "";
+  document.getElementById("hasil-keliling").innerHTML = "";
+}
+
+// Theme switcher functionality
+checkbox.addEventListener("change", () => {
+  // Toggle website theme
+  document.body.classList.toggle("dark");
+});
